360b7c419575d5b975cbf4a57a7060a53924bd54
h3xar0n <aron@glambyadelina.com>
2017-04-22 19:25:41 -0400

    two instances, sample output

--- virtual-computer.rb
+++ virtual-computer.rb
@@ -1,18 +1,28 @@
 class Computer
   @@users = {}
+
   def Computer.get_users
     @@users
   end
+
   def initialize(username, password)
     @username = username
-    @@user[username] = password
+    @@users[username] = password
     @files = {}
   end
+
   def create(filename)
     time = Time.now
     @files[filename] = time
-    puts "#{username} created a file at #{time}."
+    puts "#{filename} was created by #{@username} at #{time}."
   end
+
 end
 
 my_computer = Computer.new("Kevin Flynn","c00ld00d")
+encom_computer = Computer.new("Master Control", 56789)
+
+my_computer.create("clu.rb")
+encom_computer.create("sark.py")
+
+puts "Users: #{Computer.get_users}"
